c6ffc909ace0e032428d60d7a787368b4992c6e1
Andrey <laskarevskiy73@gmail.com>
2019-06-07 21:36:35 +0300

    Updates

---
+++ precache-manifest.a26041a8ecd70f965eb7a061695606d3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da559aed415ec87ba99b0b40c4c3eb06",
+    "url": "/goit-react-hw-03-book-search/index.html"
+  },
+  {
+    "revision": "34ecd289ca04f65c6482",
+    "url": "/goit-react-hw-03-book-search/static/css/2.fdce42ba.chunk.css"
+  },
+  {
+    "revision": "d491deee98568111b747",
+    "url": "/goit-react-hw-03-book-search/static/css/main.c7941fe9.chunk.css"
+  },
+  {
+    "revision": "34ecd289ca04f65c6482",
+    "url": "/goit-react-hw-03-book-search/static/js/2.5516ef11.chunk.js"
+  },
+  {
+    "revision": "d491deee98568111b747",
+    "url": "/goit-react-hw-03-book-search/static/js/main.09a8842f.chunk.js"
+  },
+  {
+    "revision": "94506de2c381820c5c24",
+    "url": "/goit-react-hw-03-book-search/static/js/runtime~main.5d5bdee0.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/goit-react-hw-03-book-search/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/goit-react-hw-03-book-search/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/goit-react-hw-03-book-search/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/goit-react-hw-03-book-search/static/media/notification.c0d3c94c.eot"
+  }
+]);
